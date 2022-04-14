a1b8d7fa5879725bcb42b0e0ee7f13e1dc10409f
Lee <leenock2012@yahoo.co.uk>
2022-04-14 16:39:17 +0100

    Created posts

--- scripts/users/user.js
+++ scripts/users/user.js
@@ -37,11 +37,18 @@ function User(name, username, email, phone, address, website, company, id = null
 }
 
 const userHeaders = ['id', 'name', 'username', 'email', 'phone', 'website', 'address', 'company'];
+const postsHeaders = ['userId', 'id', 'title', 'body']
 
 // containerElement is the element we want to render the table into
 // users is an array of objects representing user data
 function renderUserTable(users, containerElement) {
     const tableManager = new TableManager();
     const table = tableManager.createTable(userHeaders, users);
+    containerElement.replaceChildren(table);
+}
+
+function renderPostsTable(posts, containerElement) {
+    const tableManager = new TableManager();
+    const table = tableManager.createTable(postsHeaders, posts);
     containerElement.replaceChildren(table);
 }
