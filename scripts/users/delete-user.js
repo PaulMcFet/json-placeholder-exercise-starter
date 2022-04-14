8650b52298b23bac1931f128d74c43ad0aaf04cb
Asher Newbigging <asher.newbigging@outlook.com>
2022-04-14 16:16:50 +0100

    AN - Create deletion functionality

--- scripts/users/delete-user.js
+++ scripts/users/delete-user.js
@@ -1,1 +1,45 @@
-// TODO
+(function() {
+    const dataForm = document.querySelector('#data-form');
+    const id = document.querySelector('#id');
+
+    function findById() {
+        setStatus('Finding user to delete...')
+        fetch(`https://jsonplaceholder.typicode.com/users/${id.value}`, {
+            method: 'GET'
+        }).then(response => {
+            if (response.ok) return response.json();
+            else throw new Error('Uh oh, something went wrong...');
+        })
+          .then(user => {
+            confirm = window.confirm("Are you sure you want to delete " + user.name + "?")
+            if (confirm) {
+                remove();
+            }
+        })
+          .catch(error => {
+            setStatus('ERROR ENCOUNTERED');
+            handleError(error);
+        });
+    }
+
+    function remove() {
+        fetch(`https://jsonplaceholder.typicode.com/users/${id.value}`, {
+            method: 'DELETE'
+        }).then(response => {
+            if (response.ok) return response.json();
+            else throw new Error('Uh oh, something went wrong...');
+        }).then(json => {
+            setStatus('USER DELETED');
+            console.log(json);
+        }).catch(error => {
+            setStatus('FAILED TO DELETE USER');
+            handleError(error);
+        });
+    }
+
+    dataForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        findById();
+    });
+
+})();
